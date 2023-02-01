fb9a8726553e814252d1f62423d52ce0d462da5b
dongjoo Lee <zxcvb159852@gmail.com>
2023-02-01 18:37:03 +0900

    안뇽하세요

---
+++ src/com/greedy/section01/container/Application.java
@@ -0,0 +1,12 @@
+package com.greedy.section01.container;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+//		new JFrameTest1();
+//		new JFrameTest2().showMainFrame();
+		
+	}
+
+}
---
+++ src/com/greedy/section2/layout/run/Application.java
@@ -0,0 +1,17 @@
+package com.greedy.section2.layout.run;
+
+import com.greedy.section2.layout.A_BorderLayout;
+import com.greedy.section2.layout.B_flowLayout;
+import com.greedy.section2.layout.C_GridLayout;
+import com.greedy.section2.layout.D_CardLayout;
+
+public class Application {
+
+	public static void main(String[] args) {
+//		new A_BorderLayout();
+//		new B_flowLayout();
+//		new C_GridLayout();
+		new D_CardLayout();
+	}
+
+}
